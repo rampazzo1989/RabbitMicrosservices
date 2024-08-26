1194401c3802a0d4811ba9fee9e3b49e9726ffc6
Felipe Rampazzo <rampazzo1989@gmail.com>
2024-08-26 00:12:31 -0300

    feat: working example

--- ConsumerService/obj/Debug/net8.0/ConsumerService.AssemblyInfo.cs
+++ ConsumerService/obj/Debug/net8.0/ConsumerService.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProducerService")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsumerService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9dff267f18bb35817ed2aa71f608715a3c2be4")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProducerService")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProducerService")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsumerService")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsumerService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- ProducerService/Program.cs
+++ ProducerService/Program.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace ProducerService
 {
+    /// <summary>
+    /// Producer example (queue)
+    /// </summary>
     public class Producer
     {
         public static void Main(string[] args)
@@ -11,12 +14,12 @@ namespace ProducerService
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "testing", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-            string message = "Hello RabbitMQ!";
+            string message = "Hello RabbitMQ 3!";
             var body = Encoding.UTF8.GetBytes(message);
 
-            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            channel.BasicPublish(exchange: "", routingKey: "testing", basicProperties: null, body: body);
             Console.WriteLine(" [x] Sent {0}", message);
         }
     }
